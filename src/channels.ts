02ede89aa4bd00622731ca02471a0eb9d5970682
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2019-07-25 12:42:08 +0300

    refactoring

--- src/channels.ts
+++ src/channels.ts
@@ -1,7 +1,7 @@
-import {IChannel} from './interfaces';
+import {IChannel, Message} from './interfaces';
 
 export class RemoteWorker extends Worker implements IChannel {
-  
+
 }
 
 export class RemoteOrigin implements IChannel {
@@ -9,7 +9,7 @@ export class RemoteOrigin implements IChannel {
 
   constructor(private origin: string, private window: Window) {}
 
-  postMessage(message: any): void {
+  postMessage(message: Message): void {
     this.window.postMessage(message, this.origin);
   }
 
@@ -34,8 +34,8 @@ export class RemoteOrigin implements IChannel {
 export class RemoteSocket implements IChannel {
   constructor(private socket: WebSocket) {}
 
-  postMessage(message: any): void {
-    this.socket.send(message);
+  postMessage(message: Message): void {
+    this.socket.send(JSON.stringify(message));
   }
 
   addEventListener(type: 'message', listener: EventListener): void {
@@ -44,5 +44,21 @@ export class RemoteSocket implements IChannel {
 
   removeEventListener(type: "message", listener: EventListener): void {
     this.socket.removeEventListener('message', listener);
+  }
+}
+
+export class RemoteEvents implements IChannel {
+  constructor(private source: EventSource) {}
+
+  postMessage(message: any): void {
+    console.warn(`Sending data is not supported by EventSource`);
+  }
+
+  addEventListener(type: 'message', listener: EventListener): void {
+    this.source.addEventListener('message', listener);
+  }
+
+  removeEventListener(type: "message", listener: EventListener): void {
+    this.source.removeEventListener('message', listener);
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,15 @@
 /** @private */
+import {Message} from './interfaces';
+
 export function hasTopic(topic: string, topics: RegExp[]) {
   return Array.isArray(topics) && topics.some(t => t.test(topic));
 }
 
 /** @private */
-export function getData(msg: MessageEvent): any {
+export function getData(msg: MessageEvent): Message {
   return typeof msg.data === 'string' ? JSON.parse(msg.data) : msg.data;
+}
+
+export function uid() {
+  return Date.now().toString(36) + Math.random().toString(36);​
 }
